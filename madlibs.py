5856267bbdd1140df82630a2b9876388118a4274
Chanice Allen <75102273+chaniceallen@users.noreply.github.com>
2022-09-20 09:30:28 -0400

    First commit

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj1}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
